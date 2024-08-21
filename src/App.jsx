e804d4dfc342d9b4cb1f3d85494f09b6317a9371
Kaviya <131757368+Kaviyamurugadass@users.noreply.github.com>
2024-08-21 12:19:22 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,109 +5,102 @@ import Log from './components/Log';
 import { WINNING_COMBINATIONS } from './WinningCombination';
 import GameOver from './components/GameOver';
 
-const PLAYER = 
-  {
-    X:'Name 1',
-    O:'Name 2',
-  }
-
-const INITIAL_GAMEBOARD = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null]
-];
+const PLAYER =
+{
+  X: 'Name 1',
+  O: 'Name 2',
+}
 
 //this function is for log - return the symbol & also used for 
-function derivedplayer(truns){
+function derivedplayer(truns) {
   let currentPlayer = 'X';
-    if(truns.length > 0 && truns[0].player==='X'){
-      currentPlayer = 'O';
-    }
-    return currentPlayer;   
-}
-
-function winnerfunc(gameBoard,playeraName){
-  let winner; 
-
-for(const combination of WINNING_COMBINATIONS){
-  const fisrtSqure = gameBoard[combination[0].row][combination[0].column];
-  const secondSqure =  gameBoard[combination[1].row][combination[1].column];
-  const thirdSqure =  gameBoard[combination[2].row][combination[2].column];
-
-  if(fisrtSqure && fisrtSqure===secondSqure && secondSqure === thirdSqure){
-    winner = playeraName[fisrtSqure];
-    
+  if (truns.length > 0 && truns[0].player === 'X') {
+    currentPlayer = 'O';
   }
-}
- return winner;
-
+  return currentPlayer;
 }
 
-function derivedGameBoard(truns){
- let gameBoard = [...INITIAL_GAMEBOARD.map(array=>[...array])];
-//  let gameBoard = initialGameBoard;
+function winnerfunc(gameBoard, playeraName) {
+  let winner;
 
- for(const trun of truns){ //truns - array of objects
-   const {Index,player} = trun;
-   const {row,col} = Index;
-   gameBoard[row][col] = player;
+  for (const combination of WINNING_COMBINATIONS) {
+    const fisrtSqure = gameBoard[combination[0].row][combination[0].column];
+    const secondSqure = gameBoard[combination[1].row][combination[1].column];
+    const thirdSqure = gameBoard[combination[2].row][combination[2].column];
+
+    if (fisrtSqure && fisrtSqure === secondSqure && secondSqure === thirdSqure) {
+      winner = playeraName[fisrtSqure];
+
+    }
+  }
+  return winner;
 }
-return gameBoard;
+
+function derivedGameBoard(truns) {
+  const INITIAL_GAMEBOARD = [
+    [null, null, null],
+    [null, null, null],
+    [null, null, null]
+  ];
+
+  //  let gameBoard = [...INITIAL_GAMEBOARD.map(array=>[...array])];
+  let gameBoard = INITIAL_GAMEBOARD;
+
+  for (const trun of truns) { //truns - array of objects
+    const { Index, player } = trun;
+    const { row, col } = Index;
+    gameBoard[row][col] = player;
+  }
+  return gameBoard;
 }
 
 function App() {
 
-  
-  const [playeraName,setPlayerName] = useState(PLAYER)
-  
-  const [truns,setTurns] = useState([]);
+  const [playeraName, setPlayerName] = useState(PLAYER)
+  const [truns, setTurns] = useState([]);
 
 
   const activePlayer = derivedplayer(truns)
-  
   const gameBoard = derivedGameBoard(truns);
- 
 
 
-let winner = winnerfunc(gameBoard,playeraName);
-let draw = truns.length === 9 && !winner;    
+  let winner = winnerfunc(gameBoard, playeraName);
+  let draw = truns.length === 9 && !winner;
 
 
-  function handleSelectSqure(rIndex,cIndex){
-  
-    setTurns((prePlayer)=>{
+  function handleSelectSqure(rIndex, cIndex) {
+    setTurns((prePlayer) => {
       const UpdatedTruns = [
-        {Index:{row:rIndex, col:cIndex}, player:activePlayer},
+        { Index: { row: rIndex, col: cIndex }, player: activePlayer },
         ...prePlayer,
       ];
       return UpdatedTruns;
     });
-    
+
   }
-  // function handleGameOver(){
-  //   setTurns([]);
-  // }
-  function handleChangePlayerName(symbol,NewName){
+
+
+  function handleChangePlayerName(symbol, NewName) {
     setPlayerName(preplayer => {
-      return{
-      ...preplayer,
-      [symbol]:NewName
+      return {
+        ...preplayer,
+        [symbol]: NewName
       };
     });
   }
-  
+
   return (
-   <main>
-    <div id="game-container">
-    <ol id="players" className="highlight-player">
-        <Player name={PLAYER.X} symbol="X" isActive={activePlayer==='X'} onChangeName={handleChangePlayerName}/>
-        <Player name={PLAYER.O} symbol="O"  isActive={activePlayer==='O'} onChangeName={handleChangePlayerName}/>
-    </ol>
-    {(winner || draw) && <GameOver Winner={winner} over={()=>setTurns([])}/>}
-    
-    <GameBoard selectSymbol={handleSelectSqure} board={gameBoard}/>
-     </div>
-     <Log truns={truns} name={playeraName}/>
+    <main>
+      <div id="game-container">
+        <ol id="players" className="highlight-player">
+          <Player name={PLAYER.X} symbol="X" isActive={activePlayer === 'X'} onChangeName={handleChangePlayerName} />
+          <Player name={PLAYER.O} symbol="O" isActive={activePlayer === 'O'} onChangeName={handleChangePlayerName} />
+        </ol>
+        {(winner || draw) && <GameOver Winner={winner} over={() => setTurns([])} />}
+
+        <GameBoard selectSymbol={handleSelectSqure} board={gameBoard} />
+      </div>
+      <Log truns={truns} name={playeraName} />
     </main>
   );
 }
